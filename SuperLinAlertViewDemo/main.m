cb63f6d61dda6d2e12a18cd4d2b23f24bd725cdb
CheerHi <ZYC@zhuangyuchangdeimac.local>
2015-12-11 19:37:07 +0800

    Initial Commit

---
+++ SuperLinAlertViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperLinAlertViewDemo
+//
+//  Created by CheerHi on 15/12/11.
+//  Copyright © 2015年 MrLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
